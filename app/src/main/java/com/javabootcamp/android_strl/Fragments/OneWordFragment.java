8a2a6e2f100d068465c1f4933593975a074ea20d
roberts-p <pilaps4@inbox.lv>
2020-02-20 13:26:29 +0200

    fragment 1 progress

--- app/src/main/java/com/javabootcamp/android_strl/Fragments/OneWordFragment.java
+++ app/src/main/java/com/javabootcamp/android_strl/Fragments/OneWordFragment.java
@@ -1,13 +1,20 @@
 package com.javabootcamp.android_strl.Fragments;
 
-import android.content.pm.ActivityInfo;
+import android.content.ClipData;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.javabootcamp.android_strl.Checkable;
@@ -17,6 +24,8 @@ import java.util.List;
 
 public class OneWordFragment extends Fragment {
     private final List<String> words;
+    private TextView word1,target1;
+    int i=0;
     private Checkable checker;
 
     public OneWordFragment(List<String> words, Checkable checker) {
@@ -27,8 +36,8 @@ public class OneWordFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActivity().setRequestedOrientation(
-                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
     }
 
     @Nullable
@@ -40,5 +49,77 @@ public class OneWordFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        word1= view.findViewById(R.id.word1);
+
+
+
+        target1 = view.findViewById(R.id.target1);
+
+        word1.setOnTouchListener(touchListener);
+
+
+        target1.setOnDragListener(dragListener);
+
+
+        word1.setText(words.get(0));
     }
+
+    View.OnTouchListener touchListener = new View.OnTouchListener(){
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            ClipData data = ClipData.newPlainText("","");
+            View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(data,myShadowBuilder,v,0);
+
+            return true;
+        }
+
+    };
+    View.OnDragListener dragListener = new View.OnDragListener(){
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            final View view = (View) event.getLocalState();
+
+            switch (dragEvent){
+                case DragEvent.ACTION_DRAG_STARTED:
+                    view.setVisibility(View.INVISIBLE);
+
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    view.setVisibility(View.VISIBLE);
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+
+                    break;
+                case DragEvent.ACTION_DROP:
+
+                    if(view.getId() == R.id.word1 && v.getId() == R.id.target1){
+                        target1.setText(word1.getText());
+                        word1.setVisibility(View.GONE);
+                        /*ConstraintLayout oldparent = (ConstraintLayout) view.getParent();
+                        oldparent.removeView(view);
+                        ConstraintLayout newParent = (ConstraintLayout) v;
+                        newParent.addView(view);
+                        target1.setBackgroundColor(Color.parseColor("#00FF00"));
+                        i++;*/
+                        i++;
+                        target1.setBackgroundColor(Color.parseColor("#00FF00"));
+
+                    }
+                    if (i==1) {
+                        checker.checkCompleted();
+
+                    }
+                    break;
+            }
+            return true;
+        }
+    };
+
+
+
 }
