3d8a0bf539bdac691259d7ccbeee99b4a85cda47
Aygul Ismayilova <45090873+Aygulismayilova@users.noreply.github.com>
2020-02-24 23:53:10 +0200

    Added 2 tests for External DB.

--- app/src/test/java/com/javabootcamp/android_strl/ExampleUnitTest.java
+++ app/src/test/java/com/javabootcamp/android_strl/ExampleUnitTest.java
@@ -6,12 +6,15 @@ import android.view.MenuItem;
 //import androidx.test.rule.ActivityTestRule;
 
 import com.javabootcamp.android_strl.Adapters.TopicRecyclerViewAdapter;
+import com.javabootcamp.android_strl.ExternalDb.Corpus;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -23,11 +26,18 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
 
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void ExternalDB_Corpus_searchName_getResult() {
+        assertFalse(Corpus.searchName("skola").isEmpty());
     }
 
-
+    @Test
+    public void ExternalDB_Corpus_searchName_throwExceptionForEmpty() {
+        try {
+            List<String> list = Corpus.searchName("");
+        } catch (NullPointerException ex) {
+            assertTrue(ex instanceof NullPointerException);
+        }
+    }
 
 
 }
